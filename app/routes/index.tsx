41b0558f8d3889cb66d7b219e080634214a65918
Shane <shane@swalker.dev>
2022-05-21 09:05:15 -0500

    fixed error

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,14 +2,15 @@ import { useFetcher } from "@remix-run/react";
 import { useEffect, useState } from "react";
 
 export default function Index() {
-  const [fetchCount, setFetchCount] = useState(0);
+  const [year, setYear] = useState();
+  const [id, setId] = useState();
+  const [color, setColor] = useState();
 
   const trollyData = useFetcher();
 
   useEffect(() => {
     if (trollyData.type === "init") {
       trollyData.load("/trollyUpdate/");
-      setFetchCount(fetchCount + 1)
     }
   }, [trollyData]);
 
@@ -19,20 +20,13 @@ export default function Index() {
       trollyData.load("/trollyUpdate/");
     }, 3000);
   }, []);
-  console.log("trollyData.data :", trollyData.data); ////eslint disable line ##DEBUG
+  console.log("trollyData.data :", trollyData.data?.data); ////eslint disable line ##DEBUG
   return (
-    <div
-      style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}
-    >
-      Fetches: {fetchCount}
-
-      raw: {
-        JSON.stringify(trollyData.data)
-      }
-      id: {trollyData.data.data.id}
-      name: {trollyData.data.data.name}
-      year: {trollyData.data.data.years}
-
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <div>raw: {JSON.stringify(trollyData.data?.data)}</div>
+      <div>id: {trollyData.data?.data.id}</div>
+      <div>name: {trollyData.data?.data.name}</div>
+      <div>year: {trollyData.data?.data.year}</div>
     </div>
   );
 }
